18e255dd38803f7ab8c95c6320d32bdfd08205bf
Matt Hutton <mbhutton@gmail.com>
2025-03-16 20:41:54 +1300

    swiftformat of Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "EmacsOpen",
     platforms: [
-        .macOS(.v12)
+        .macOS(.v12),
     ],
     products: [
         .library(name: "EmacsOpenLibrary", targets: ["EmacsOpenLibrary"]),
@@ -12,7 +12,7 @@ let package = Package(
 
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         .target(
