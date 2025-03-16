72fdddab335c09f79512b0f5bf51a172eb0cf718
Matt Hutton <mbhutton@gmail.com>
2025-03-16 23:34:08 +1300

    formatting

--- Package.swift
+++ Package.swift
@@ -3,28 +3,17 @@ import PackageDescription
 
 let package = Package(
     name: "EmacsOpen",
-    platforms: [
-        .macOS(.v12)
-    ],
+    platforms: [.macOS(.v12)],
     products: [
         .library(name: "EmacsOpenLibrary", targets: ["EmacsOpenLibrary"]),
         .executable(name: "emacsopen", targets: ["EmacsOpenCLI"]),
-
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
-    ],
+    dependencies: [.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")],
     targets: [
-        .target(
-            name: "EmacsOpenLibrary",
-            dependencies: []
-        ),
+        .target(name: "EmacsOpenLibrary", dependencies: []),
         .executableTarget(
             name: "EmacsOpenCLI",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                "EmacsOpenLibrary",
-            ]
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser"), "EmacsOpenLibrary"]
         ),
     ]
 )
