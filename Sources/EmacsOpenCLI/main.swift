d23f6ea05b0d97fb564a8ca769ffea3efd8e4055
Matt Hutton <mbhutton@gmail.com>
2025-03-16 23:57:16 +1300

    DRAFT main.swift updates

--- Sources/EmacsOpenCLI/main.swift
+++ Sources/EmacsOpenCLI/main.swift
@@ -3,39 +3,21 @@ import Darwin
 import EmacsOpenLibrary
 import Foundation
 
-let execClause: String = "\nReplaces process with emacsclient process."
-let incompatibleClause: String = "\nIncompatible with all other OPTIONS."
-
 struct EmacsOpenCLI: ParsableCommand {
-    static let configuration = CommandConfiguration(
-        commandName: "emacsopen",
-        abstract: "Open files and links in Emacs"
-    )
+    static let configuration = CommandConfiguration(commandName: "emacsopen", abstract: "Open files and links in Emacs")
 
-    @Flag(
-        name: .shortAndLong,
-        help: "Block until buffer is closed.\nRequires at least one <file> or <link> argument."
-    )
-    var block: Bool = false
-    @Flag(name: .shortAndLong, help: "Create a new frame.")
-    var createFrame: Bool = false
-    @Flag(
-        name: .shortAndLong,
-        help:
-        "Evaluate Emacs Lisp commands.\nRequires exactly one <command> argument.\(incompatibleClause)\(execClause)"
-    )
-    var eval: Bool = false
-    @Flag(name: .shortAndLong, help: "Open files in terminal.\(incompatibleClause)\(execClause)")
-    var tty: Bool = false
-    @Argument(help: "File(s) or org-protocol link to open, or single command in the --eval case.")
-    var files: [String] = []
+    @Flag(name: .shortAndLong, help: "Block until buffer is closed.") var block: Bool = false
+    @Flag(name: .shortAndLong, help: "Create a new frame.") var createFrame: Bool = false
+    @Flag(name: .shortAndLong, help: "Evaluate Emacs Lisp commands.") var eval: Bool = false
+    @Flag(name: .shortAndLong, help: "Open files in terminal.") var tty: Bool = false
+    @Argument(help: "File(s) or org-protocol link to open.") var files: [String] = []
 
     func run() throws {
         if tty && (block || createFrame || eval) {
-            failAndExit("The --tty option is incompatible with all other OPTIONS\n")
+            failAndExit("The --tty option is incompatible with --block, --create-frame, and --eval\n")
         }
         if eval && (tty || block || createFrame) {
-            failAndExit("The --eval option is incompatible with all other OPTIONS\n")
+            failAndExit("The --eval option is incompatible with --tty, --block, and --create-frame\n")
         }
         if eval {
             if files.count == 1 {
@@ -47,8 +29,7 @@ struct EmacsOpenCLI: ParsableCommand {
             failIfFalse(openInTerminal(filesOrLink: files))
         } else {
             if block && files.isEmpty {
-                failAndExit(
-                    "The --block option requires at least one file or org-protocol link argument\n")
+                failAndExit("The --block option requires at least one file or org-protocol link argument\n")
             }
             if files.isEmpty {
                 failIfFalse(activateFrame(createFrame: createFrame))
@@ -65,10 +46,6 @@ func failAndExit(_ message: String) {
     Darwin.exit(1)
 }
 
-func failIfFalse(_ flag: Bool) {
-    if !flag {
-        Darwin.exit(1)
-    }
-}
+func failIfFalse(_ flag: Bool) { if !flag { Darwin.exit(1) } }
 
 EmacsOpenCLI.main()
