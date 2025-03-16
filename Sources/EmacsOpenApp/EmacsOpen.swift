37c84b3025bdbe0f27f4bbe33291dede0a3c8f70
Matt Hutton <mbhutton@gmail.com>
2025-03-16 23:37:25 +1300

    main.swift moved to EmacsOpen.swift for app, not yet restored features

---
+++ Sources/EmacsOpenApp/EmacsOpen.swift
@@ -0,0 +1,73 @@
+import SwiftUI
+
+@main struct MenuBarApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        MenuBarExtra("My App", systemImage: "link") {
+            VStack {
+                Text("My Share Extension App").padding()
+
+                Divider()
+
+                Button("Quit") {
+                    print("kbye")
+                    NSApplication.shared.terminate(nil)
+                }.padding()
+            }
+        }.menuBarExtraStyle(.window)
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_: Notification) { print("Hello World from SwiftUI menu bar app!") }
+}
+
+/*
+ @objc(AppDelegate) // Make sure this is @objc to be recognized as the app delegate.
+ class AppDelegate: NSObject, NSApplicationDelegate {
+
+     let path = "/Users/matt/sw/emacsopen/emacsopen"
+
+     func applicationDidFinishLaunching(_ notification: Notification) {
+         print("hello world app launched")
+         runCommand("\(path)")
+         NSApp.terminate(nil)
+     }
+
+     func applicationWillTerminate(_ notification: Notification) {
+         print("bye")
+         // Cleanup before app terminates
+     }
+     func application(_ application: NSApplication, open urls: [URL]) {
+         for url in urls {
+             if url.scheme == "org-protocol" {
+                 runCommand("\(path) emacsopen \(url.absoluteString)")
+             } else {
+                 runCommand("\(path) eemacsopen \(url.path)")
+             }
+         }
+         NSApp.terminate(nil)
+     }
+
+     func runCommand(_ command: String) {
+         let task = Process()
+         task.launchPath = "/bin/bash"
+         task.arguments = ["-c", command]
+         task.launch()
+         task.waitUntilExit()
+     }
+ }
+
+ func runCommand(_ command: String) {
+     let task = Process()
+     task.launchPath = "/bin/bash"
+     task.arguments = ["-c", command]
+     task.launch()
+     task.waitUntilExit()
+ }
+ let path = "/Users/matt/sw/emacsopen/emacsopen"
+ runCommand("echo hello")
+ runCommand(path)
+
+ */
